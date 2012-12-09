67ce31507c05f7bfc42574f3fa4379a3a66b58b4
Dave Lafferty <laffercd@djangomango.home>
2012-12-08 19:20:51 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+
+setup(
+    name='cekolabs_django_widgets',
+    version='0.1.0',
+    author='Dave Lafferty',
+    author_email='info@dave-lafferty.com',
+    packages=['cekolabs_django_widgets',],
+    scripts=[],
+    url='http://dave-lafferty.com/python-packages/cekolabs_django_widgets', #TODO: Switch to github
+    license='LICENSE.txt',
+    description='All the django widgets used on www.dave-lafferty.com.',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "Django >= 1.4.2",        
+    ],
+)
