30c85323f01478fa20d2baa429019eefa93ec09d
Dave Lafferty <laffercd@djangomango.home>
2012-12-09 19:05:52 -0500

    Widget added and dependencies updated.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='cekolabs_django_widgets',
-    version='0.1.0',
+    version='0.1.1',
     author='Dave Lafferty',
     author_email='info@dave-lafferty.com',
     packages=['cekolabs_django_widgets',],
@@ -13,6 +13,7 @@ setup(
     description='All the django widgets used on www.dave-lafferty.com.',
     long_description=open('README.txt').read(),
     install_requires=[
-        "Django >= 1.4.2",        
+        "Django >= 1.4.2", 
+        "markdown",       
     ],
 )
